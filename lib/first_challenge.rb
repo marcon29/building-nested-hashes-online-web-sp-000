c0922f6defd80b7fe8f5e7ec2fce8f20116ef263
Mark Stabler <mark@markstabler.com>
2020-03-09 22:52:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,34 +1,3 @@
-#def first_challenge
-#   epic_tragedy = {}
-#end
-
-epic_tragedy = {
-  montagues: {
-    patriarch: {
-      name: "Lord Montague",
-      age: 53
-    },
-    matriarch: {
-      name: "Lady Montague",
-      age: 54
-    },
-    hero: {
-      name: "Romeo",
-      age: 15,
-      status: "Alive"
-    },
-    hero_friends: {
-      friend_1: {
-        name: "Benvolio",
-        age: 17,
-        attitude: "worried"
-      },
-      friend_2: {
-        name: "Mercutio",
-        age: 18,
-        attitude: "hot-headed"
-      }
-    }
-  }
-}
-puts epic_tragedy
+def first_challenge
+   epic_tragedy = {}
+end
